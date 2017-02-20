d38cbc00adf428e188cb8252af0023ac545e0f4a
Jasper <jasper841124@gmail.com>
2017-02-20 23:13:01 +0800

    add camera

--- OpenCV_Practice/main.cpp
+++ OpenCV_Practice/main.cpp
@@ -18,19 +18,32 @@ int main()
 
 	Mat frame;
 	VideoCapture cap("Blue Exorcist.mp4");
+	VideoCapture cap2(0);
 
 	if (!cap.isOpened()) {
 		cout << "Can't open the video!" << endl;
 		return -1;
 	}
+	if (!cap2.isOpened()) return -1;
+
+	Mat edges;
+	namedWindow("my camera", 1);
 
 	for (;;) {
 		cap.read(frame);
+		Mat frame2;
+		cap2 >> frame2;
 
-		if (frame.rows > 0) imshow("Video Frame", frame);
-		else break;
+		Mat ROI = frame(Rect(0, 0, frame2.cols, frame2.rows));
+		addWeighted(ROI, 0.5, frame2, 0.3, 0, ROI);
 
-		waitKey(30);
+		//if (frame.rows > 0) imshow("my camera", frame);
+		//else break;
+
+		imshow("my camera", frame);
+
+		//waitKey(30);
+		if (waitKey(30) == 27) break;
 	}
 	return 0;
 }
